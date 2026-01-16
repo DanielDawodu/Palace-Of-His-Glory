2a04d2e406e4a7d34601e72b29ba8175b6a6d027
Marvellous Obama <marvellousobama@gmail.com>
2026-01-16 11:11:22 +0100

    fix(db): enforce ssl for production connection

--- server/db.ts
+++ server/db.ts
@@ -11,7 +11,10 @@ if (!process.env.DATABASE_URL) {
 }
 
 export const pool = process.env.DATABASE_URL
-  ? new Pool({ connectionString: process.env.DATABASE_URL })
+  ? new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false }
+  })
   : null;
 export const db = pool ? drizzle(pool, { schema }) : null;
 
